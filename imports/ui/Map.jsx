2808bfc4d3f66ad769b275ec0a6d8393530854b2
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2017-05-16 10:48:45 -0500

    Incial

---
+++ imports/ui/Map.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+export default class Map extends Component {
+
+	
+
+
+render() {
+		return (
+			<div className="canvasTweets">
+		<canvas id="myCanvas" width="600" height="600"></canvas>
+			</div>);
+	}
+}
+
+
